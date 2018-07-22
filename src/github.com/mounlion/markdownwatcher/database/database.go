bbc91c4a4a9d8b9fc8395d48efac8e2a303c2901
Fonov Sergei <ask@mounlion.com>
2018-07-22 21:14:59 +0300

    make right database connect

--- src/github.com/mounlion/markdownwatcher/database/database.go
+++ src/github.com/mounlion/markdownwatcher/database/database.go
@@ -7,93 +7,59 @@ import (
 	"log"
 )
 
-
-func main () {
-	var Items []parsing.Item
-	Items = append(Items, parsing.Item{"Title", "Url", "sha256", "2222", 12, 1200})
-
-	PrepareItems(Items)
-}
-
-type CatalogData struct {
-	Id string
-	Price int
-}
-
 func PrepareItems(items []parsing.Item) ([]parsing.Item, []parsing.Item) {
-	// получить список объектов по идексам в воде мы должны получит массив объектов ид цена
-	// расчитывем какие новые а какие обновленные
-	// insert новые объекты
-	// update объекты для обновление
-	// возврашаем новые и обновленные оъеты
-
 	db, err := sql.Open("sqlite3", "./MarkDownWatcher.db")
 	CheckErr(err)
 	defer db.Close()
 
-	sqlStr := "select id, price from items where id in ("
+	selectStr := "select id, price from items where id in ("
+	selectIds := make([]interface{}, len(items))
 
 	for i, val := range items {
-		sqlStr += `"`+val.ItemId+`"`
+		selectStr += "?"
+		selectIds[i] = val.ItemId
 		if len(items)-1 != i {
-			sqlStr += ","
+			selectStr += ","
 		} else {
-			sqlStr += ")"
+			selectStr += ")"
 		}
 	}
 
-	rows, err := db.Query(sqlStr)
+	rows, err := db.Query(selectStr, selectIds...)
 	if err != nil {log.Fatal(err)}
 	defer rows.Close()
 
 	var (
 		id string
 		price int
-		catalogsData []CatalogData
 		updateItems []parsing.Item
-		newItems []parsing.Item
-		isFirstRun = false
+		CountRows int
 	)
 
 	for rows.Next() {
 		err = rows.Scan(&id, &price)
 		CheckErr(err)
-		catalogsData = append(catalogsData, CatalogData{id, price})
-	}
-
-	if len(catalogsData) > 0 {
-		for _, val := range items {
-			isNewItem := true
-			for _, dbVal := range catalogsData {
-				if val.ItemId == dbVal.Id && val.Price == dbVal.Price {
-					isNewItem = false
-					break
+		for i, item := range items {
+			if item.ItemId == id {
+				if item.Price != price {
+					updateItems = append(updateItems, item)
 				}
-				if val.ItemId == dbVal.Id && val.Price != dbVal.Price {
-					isNewItem = false
-					updateItems = append(updateItems, val)
-					break
-				}
-			}
-
-			if isNewItem {
-				newItems = append(newItems, val)
+				items = append(items[:i], items[i+1:]...)
+				break
 			}
 		}
-	} else {
-		isFirstRun = true
-		newItems = items
+		CountRows++
 	}
 
-	if len(newItems) > 0 || len(updateItems) > 0 {
+	if len(items) > 0 || len(updateItems) > 0 {
 		tx, err := db.Begin()
 		if err != nil {log.Fatal(err)}
 
-		if len(newItems) > 0 {
+		if len(items) > 0 {
 			stmt, err := tx.Prepare("INSERT OR IGNORE INTO items(id, title, url, description, price, oldPrice) values (?,?,?,?,?,?)")
 			if err != nil {log.Fatal(err)}
 			defer stmt.Close()
-			for _, item := range newItems {
+			for _, item := range items {
 				_, err = stmt.Exec(item.ItemId, item.Title, item.Url, NullString(item.Desc), item.Price, NullInt(item.OldPrice))
 				if err != nil {log.Fatal(err)}
 			}
@@ -102,7 +68,7 @@ func PrepareItems(items []parsing.Item) ([]parsing.Item, []parsing.Item) {
 			stmt, err := tx.Prepare("UPDATE items set price=? where id=?")
 			if err != nil {log.Fatal(err)}
 			defer stmt.Close()
-			for _, item := range newItems {
+			for _, item := range updateItems {
 				_, err = stmt.Exec(item.Price, item.ItemId)
 				if err != nil {log.Fatal(err)}
 			}
@@ -111,10 +77,10 @@ func PrepareItems(items []parsing.Item) ([]parsing.Item, []parsing.Item) {
 		tx.Commit()
 	}
 
-	if isFirstRun {
-		return []parsing.Item{}, []parsing.Item{}
+	if CountRows > 0 {
+		return items, updateItems
 	} else {
-		return newItems, updateItems
+		return []parsing.Item{}, []parsing.Item{}
 	}
 }
 
