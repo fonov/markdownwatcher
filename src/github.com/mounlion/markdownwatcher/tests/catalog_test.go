f5bee00ee828f644c0ea2ae4fe4d83f0b211e2de
Sergei Fonov <sfonov@qleanlabs.ru>
2018-07-23 14:40:22 +0300

    test

--- src/github.com/mounlion/markdownwatcher/tests/catalog_test.go
+++ src/github.com/mounlion/markdownwatcher/tests/catalog_test.go
@@ -11,8 +11,9 @@ import (
 func TestPrepareCatalog(t *testing.T) {
 
 	catalog := []parsing.Item{
-		parsing.Item{"be40e256-898c-4480-bea5-fc86fd23b45d", "GPS навигатор DEXP Auriga DS503", "", "/catalog/markdown/be40e256-898c-4480-bea5-fc86fd23b45d/", 1200, 2000},
-		parsing.Item{"be40e256-898c-4480-bea5-fc86fd23b45d-12", "GPS навигатор DEXP Auriga DS503", "", "/catalog/markdown/be40e256-898c-4480-bea5-fc86fd23b45d/", 1300, 2000},
+		parsing.Item{"be40e256-898c-4480-bea5-fc86fd23b45d-ld", "GPS навигатор DEXP Auriga DS503", "", "/catalog/markdown/be40e256-898c-4480-bea5-fc86fd23b45d/", 1200, 2000},
+		parsing.Item{"3de723fd-bf83-4af8-a792-4a59bfe909b4", "GPS навигатор DEXP Auriga DS503", "", "/catalog/markdown/be40e256-898c-4480-bea5-fc86fd23b45d/", 1300, 2000},
+		parsing.Item{"7000b444-087b-4d8b-a762-8ba56088a98f", "GPS навигатор DEXP Auriga DS503", "", "/catalog/markdown/be40e256-898c-4480-bea5-fc86fd23b45d/", 2890, 2000},
 	}
 
 	newItems, updateItems := database.PrepareItems(catalog)
